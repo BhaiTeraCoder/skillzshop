dbb217de8c7ea768dc08657ea352d6d55db69721
Aesha Ganatra <40544440+aeshaganatra@users.noreply.github.com>
2022-11-19 14:15:00 -0500

    Create Project

---
+++ skillzshop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace skillzshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ skillzshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace skillzshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
