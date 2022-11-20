640c6ac74b7dded788f0e067e53f22524c7284f2
apple-coleene <acduquinal@gmail.com>
2022-11-19 23:13:51 -0500

    Creating search result page and payment option

---
+++ skillzshop/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace skillzshop.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
