ca0a2ad6c5cb611b5cdc2e3eff9bad1c9df6f833
apple-coleene <acduquinal@gmail.com>
2022-12-04 20:00:00 -0500

    Skills and Categories integration

--- skillzshop/Controllers/HomeController.cs
+++ skillzshop/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using skillzshop.Data;
 
 namespace skillzshop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext appDbContext;
+
+        public HomeController(AppDbContext context)
+        {
+            appDbContext = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var data = appDbContext.Category.ToList();
+            return View(data);
         }
 
 
