c3dc018641825bdd969e9e21ceb810b528bc9ac5
Jitin-Gambhir <Jgambhi3@my.centennialcollege.ca>
2023-03-10 15:55:44 -0500

    Added Category model and fix payment button

---
+++ skillzshop/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using skillzshop.Data;
+using skillzshop.Models;
+
+namespace skillzshop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var allCategory = _context.Category;
+            return View(allCategory.ToList());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(String NameSearch)
+        {
+            ViewData["getCategory"] = NameSearch;
+
+            var categoryquery = from x in _context.Category select x;
+            if (!String.IsNullOrWhiteSpace(NameSearch))
+            {
+                categoryquery = categoryquery.Where(x => x.CategoryId.Equals(NameSearch));
+            }
+            return View(await categoryquery.AsNoTracking().ToListAsync());
+        }
+
+
+        //Get: Category/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var categoryDetails = await _context.Category.FirstOrDefaultAsync(n => n.CategoryId == id);
+            if (categoryDetails == null) return View("Empty");
+            return View(categoryDetails);
+        }
+    }
+}
---
+++ skillzshop/Controllers/PaymentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using skillzshop.Data;
+using skillzshop.Models;
+
+namespace skillzshop.Controllers
+{
+    public class PaymentController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public PaymentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Purchase(int skillId)
+        {
+            // Code to process payment and complete purchase goes here
+            // You could use a payment gateway API, like Stripe or PayPal, to handle the payment process
+
+            // Redirect the user to a thank you page after the purchase is complete
+            return RedirectToAction("ThankYou", "Payment");
+        }
+
+
+        public IActionResult ThankYou()
+        {
+            // Code to display thank you page after the purchase is complete goes here
+            return View();
+        }
+    }
+}
