3b5df4bd30e9f5e1788f7f2ed36cd4b240e64dfd
Aesha Ganatra <40544440+aeshaganatra@users.noreply.github.com>
2023-03-16 14:23:40 -0400

    changed UI

---
+++ skillzshop/Migrations/20230316171725_aesha-migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace skillzshop.Migrations
+{
+    public partial class aeshamigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
